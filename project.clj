772a891ea1c6a6bb085cebe182e17c3a2fc7d113
tormaroe <torbjorn.maro@gmail.com>
2011-06-01 11:24:34 +0200

    Bumped version to 0.3.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pswincom-clj "0.3.3"
+(defproject pswincom-clj "0.3.4"
   :description "PSWinCom SMS client library"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
